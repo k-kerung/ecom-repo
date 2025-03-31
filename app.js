407ce459e3f2157fd75b3feff4435e3e31ed5e1e
k-kerung <kk573@canterbury.ac.uk>
2025-03-31 08:58:29 +0100

    Add transitions to app.js and style.css

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world!")
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
